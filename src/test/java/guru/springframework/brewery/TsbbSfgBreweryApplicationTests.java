537e9cbe9132f177602b80d3000571cca1660da6
ChefServiceInfo <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>
2023-12-11 14:56:09 +0100

    Updating Spring Boot 2.1.x to JUnit 5.

--- src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
+++ src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
@@ -1,16 +1,13 @@
 package guru.springframework.brewery;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class TsbbSfgBreweryApplicationTests {
+class TsbbSfgBreweryApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
