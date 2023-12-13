ee4c11132df7e28d80bb95b1f62de3876a1b0fe9
ChefServiceInfo <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>
2023-12-13 17:04:36 +0100

    Correcting Wiremock Dependency, and Using Wiremock for Testing.

---
+++ src/test/java/guru/springframework/brewery/events/BeerOrderStatusChangeEventListenerTest.java
@@ -0,0 +1,59 @@
+package guru.springframework.brewery.events;
+
+import com.github.jenspiegsa.wiremockextension.Managed;
+import com.github.jenspiegsa.wiremockextension.WireMockExtension;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import guru.springframework.brewery.domain.BeerOrder;
+import guru.springframework.brewery.domain.OrderStatusEnum;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import static com.github.jenspiegsa.wiremockextension.ManagedWireMockServer.with;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+/**
+ * Created by Rami SAHRAOUI on 13/12/2023
+ */
+@ExtendWith(WireMockExtension.class)
+class BeerOrderStatusChangeEventListenerTest {
+
+    @Managed
+    WireMockServer wireMockServer = with(wireMockConfig().dynamicPort());
+
+    BeerOrderStatusChangeEventListener listener;
+
+    @BeforeEach
+    void setUp() {
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        listener = new BeerOrderStatusChangeEventListener(restTemplateBuilder);
+    }
+
+    @Test
+    void listen() {
+        // given
+        wireMockServer.stubFor(
+                post("/update")
+                        .willReturn(ok())
+        );
+
+        BeerOrder beerOrder = BeerOrder.builder()
+                .orderStatus(OrderStatusEnum.READY)
+                .orderStatusCallbackUrl("http://localhost:" + wireMockServer.port() + "/update")
+                .createdDate(Timestamp.valueOf(LocalDateTime.now()))
+                .build();
+
+        BeerOrderStatusChangeEvent event = new BeerOrderStatusChangeEvent(beerOrder, OrderStatusEnum.NEW);
+
+        // when
+        listener.listen(event);
+
+        // then
+        verify(1, postRequestedFor(urlEqualTo("/update")));
+    }
+}
